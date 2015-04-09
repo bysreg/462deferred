7321ffd827bb762d44dce2ffbf75a246b8cdb1a0
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2015-04-08 23:21:13 -0400

    early implementation of displaying model

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -32,28 +32,38 @@ void Renderer::render( const Camera& camera, const Scene& scene )
 	glLoadIdentity();
 	gluLookAt(camera.get_position().x, camera.get_position().y, camera.get_position().z, cameraTarget.x, cameraTarget.y, cameraTarget.z, camera.get_up().x, camera.get_up().y, camera.get_up().z);
 
-	glPushMatrix();
-
-	glTranslated(pos.x, pos.y, pos.z);	
-	glScaled(0.5f, 0.5f, 0.5f);
-
 	const StaticModel* staticModels = scene.get_static_models();
 	for (int i = 0; i < scene.num_static_models(); i++)
 	{
-		//glTranslatef(staticModels.)
-		//staticModels[i].
+		glPushMatrix();
+
+		glTranslatef(staticModels[i].position.x, staticModels[i].position.y, staticModels[i].position.y);
+		//glRotatef(staticModels[i].orientation.x, staticModels[i].orientation.y, staticModels[i].position.z);
+		glScalef(staticModels[i].scale.x, staticModels[i].scale.y, staticModels[i].scale.z);
+		
+
+
+		glPopMatrix();
 	}
 
-	glBegin(GL_TRIANGLES);
-	glColor3f(0, 1, 0);
-	glVertex3f(-1, -1, 0);
-	glColor3f(1, 0, 0);
-	glVertex3f(1, -1, 0);
-	glColor3f(0, 0, 1);
-	glVertex3f(0, 1, 0);
-	glEnd();
+	{
+		glPushMatrix();
+		//simple triangle
+		glTranslated(pos.x, pos.y, pos.z);
+		glScaled(0.5f, 0.5f, 0.5f);
+
+		glBegin(GL_TRIANGLES);
+		glColor3f(0, 1, 0);
+		glVertex3f(-1, -1, 0);
+		glColor3f(1, 0, 0);
+		glVertex3f(1, -1, 0);
+		glColor3f(0, 0, 1);
+		glVertex3f(0, 1, 0);
+		glEnd();
+
+		glPopMatrix();
+	}
 	
-	glPopMatrix();
 }
 
 void Renderer::release()
