02a0fe5a93097800c0d84db94d8da7cf412edb11
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2015-04-19 22:31:00 -0400

    added varying v_posW in the shader display world position

--- shaders/simple_triangle.fs
+++ shaders/simple_triangle.fs
@@ -4,13 +4,15 @@ varying vec4 v_color;
 varying vec4 v_posP;
 varying vec2 v_uv;
 varying vec3 v_normalL;
+varying vec4 v_posW;
 
 uniform sampler2D u_diffuse_texture;
 
 void main()
 {	
-	//gl_FragColor = vec4(v_posP.z / 20.0); // display depth, 
-	gl_FragColor = texture2D(u_diffuse_texture, v_uv); // display diffuse
+	//gl_FragColor = vec4(v_posP.z / 20.0); // display depth, divided by 20 so that it will display more progressive 
+	//gl_FragColor = texture2D(u_diffuse_texture, v_uv); // display diffuse
 	//gl_FragColor = vec4(v_uv, 0.0, 1.0); // display uv
 	//gl_FragColor = vec4(v_normalL, 1.0); // display normal
+	gl_FragColor = vec4(v_posW.xyz, 1.0); // display world position
 }
--- shaders/simple_triangle.vs
+++ shaders/simple_triangle.vs
@@ -5,6 +5,7 @@ attribute vec3 a_normalL;
 varying vec4 v_posP;
 varying vec2 v_uv;
 varying vec3 v_normalL;
+varying vec4 v_posW;
 
 uniform mat4 u_world;
 uniform mat4 u_proj_view;
@@ -14,7 +15,7 @@ void main()
 	v_uv = a_uv;
 	v_normalL = a_normalL;
 	
-	vec4 posW = vec4(a_posL, 1.0);
-	v_posP = u_proj_view * u_world * posW;
+	v_posW = u_world * vec4(a_posL, 1.0);
+	v_posP = u_proj_view * v_posW;
 	gl_Position = v_posP;
 }
